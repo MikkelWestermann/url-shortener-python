87ecd33efcdfeec0dc9f167a6a454ca8d1ec74f5
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-04-21 12:31:03 +0200

    Add create tiny route

--- server.py
+++ server.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+import re
+import uuid
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world(): 
-  print('Shit has been hit')
-  return 'Hello World!!!!'
- 
+  return 'Working!'
+
+
+@app.route('/create-tiny', methods=['POST'])
+def create_tiny(): 
+  if request.method == 'POST':
+    url, duration = request.get_json().values()
+    if re.match("^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$", url): 
+      safety = 0
+      while safety < 100: 
+        url_code = uuid.uuid4()
+        print(request.get_json())
+        print(url_code)
+        safety += 1
+      return 'working!'
+    else: 
+      return 'not a valid url'
